62e60996d333e9fcc7ec384a332052aab6bfed88
Aaron Hu <aaron@ip-192-168-1-6.us-west-2.compute.internal>
2021-10-15 10:53:58 -0700

    added error handling for 400 and 500s errors.

--- script.py
+++ script.py
@@ -22,20 +22,31 @@ for each in person_ids:
     print(str(current_count) + "/"+ str(total_count) + " Working on " + str(each) + " - "+ str(response.json()))
     log_file.write(str(each) + ", " + str(response.status_code) + ", " + str(response.json()))
     log_file.write("\n")
+  elif response.status_code == 422:
+    print(str(current_count) + "/"+ str(total_count) + " Working on " + str(each) + " - "+ str(response.json()))
+    log_file.write(str(each) + ", " + str(response.status_code) + ", " + str(response.json()))
+    log_file.write("\n")
   elif response.status_code == 429:
-  	print("Working on " + str(each) + " - "+ str(response.json()))
-  	break
+    print("Working on " + str(each) + " - 429 error - "+ str(response.json()))
+    print(str(current_count) + "/"+ str(total_count) + " Working on " + str(each) + " - API Limit Reached")
+    log_file.write(str(each) + ", " + str(response.status_code) + ", " + "API Limit Reached")
+    log_file.write("\n")
+    break
   elif response.status_code == 500:
-  	print("Working on " + str(each) + " - "+ str(response.json()))
-  	break
+    print("Working on " + str(each) + " - 500 error - Internal Server Error")
+    print(str(current_count) + "/"+ str(total_count) + " Working on " + str(each) + " - Internal Server Error")
+    log_file.write(str(each) + ", " + str(response.status_code) + ", " + "Internal Server Error")
+    log_file.write("\n")
   elif response.status_code == 503:
-  	print("Working on " + str(each) + " - "+ str(response.json()))
-  	break
+    print(str(current_count) + "/"+ str(total_count) + " Working on " + str(each) + " - Internal Server Error")
+    log_file.write(str(each) + ", " + str(response.status_code) + ", " + "Internal Server Error")
+    log_file.write("\n")
   else:
   	print(str(current_count) + "/"+ str(total_count) + " Working on " + str(each) + " - "+ str(response.json()))
   	log_file.write(str(each) + ", " + str(response.status_code) + ", " + response.json()[0])
   	log_file.write("\n")
 
 log_file.write("\n")
-log_file.write("Number of successful person removed: " + str(corrected_count))
+log_file.write("Number of successful person removed: " + str(corrected_count)+"\n")
+log_file.write("Number of entries proccessed: " + str(current_count))
 log_file.close()
--- test.py
+++ test.py
@@ -1,3 +1,9 @@
+import requests
+import csv
+
+from apikeys import *
+from person_ids import *
+
 # from __future__ import division
 
 # total_count = len(person_id)
@@ -5,3 +11,6 @@
 # corrected_count = 0
 
 # print("{0:.0f}%".format(current_count/total_count * 100))
+
+response = requests.delete("https://api.affinity.co/persons/64192646", auth=('', api_key))
+print("Working on " + " - " + str(response.json()))
